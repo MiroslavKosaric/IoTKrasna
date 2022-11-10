317017b30fea6394747833db4331a7dd2b663bd6
miRx923 <miroslav.kosari@student.tuke.sk>
2022-11-10 21:14:29 +0100

    edit index.php

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
-    echo '<link rel="stylesheet" type="text/css" href="style.css"></head>';
-
-    echo '<h1>Test</h1>'
+    
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>IPaIoT Web Form</title>
+</head>
+<body>
+    <h1>Test</h1>'
+</body>
+</html>
