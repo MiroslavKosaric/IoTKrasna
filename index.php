d7fbeb43b6dfd032026b4851eccace6a7e668686
miRx923 <m.kosaric@protonmail.com>
2022-11-11 10:53:26 +0100

    skuska

--- index.php
+++ index.php
@@ -1,4 +1,53 @@
 <!DOCTYPE html>
+
+<?php
+    $name = $_POST['name'];
+    $surname = $_POST['surname'];
+    
+    if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
+        $nameError = "Tvoje krstné meno nie je zapísané správne";
+    } elseif (empty($_POST['name'])) {
+        $nameError = "Musíš zadať krstné meno";
+    }
+
+    if (!preg_match("/^[a-zA-Z-' ]*$/", $surname)) {
+        $nameError = "Tvoje priezvisko nie je zapísané správne.";
+    } elseif (empty($_POST['surname'])) {
+        $nameError = "Musíš zadať priezvisko";
+    }
+
+    $num = $_POST['num'];
+    $email = $_POST['email'];
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $emailError = "Email zapísaný v nesprávnom formáte.";
+    } elseif (empty($_POST['email'])) {
+        $emailError = "Musíš zadať email."
+    }
+
+    if (isset($_POST['isLiked'])) {
+        $isLiked = "Ano";
+    } elseif (isset($_POST['isNotLiked'])) {
+        $isLiked = "Nie";
+    } else {
+        $isLiked = "";
+    }
+
+    $favColor = $_POST['favColor'];
+    $opinion = $_POST['opinion'];
+
+    $text = "Meno: " . $name . " " . $surname . "\r\n";
+    $text .= "Telefonne cislo: " . $num . "\r\n";
+    $text .= "Email: " . $email . "\r\n";
+    $text .= "Paci sa mu/jej IPaIoT: " . $isLiked . "\r\n";
+    $text .= "Nazor na predmet: " . $opinion;
+
+    $data = fopen("data.txt", "w") or die("Unable to open file!");
+    fwrite($data, $text);
+
+    fclose($data);
+?>
+
 <html lang="sk">
 <head>
     <meta charset="UTF-8">
@@ -10,7 +59,7 @@
 <body>
     <div class="formDiv">
         <h2 class="heading">IPAIOT WEB FORM</h2>
-        <form action="getData.php" method="post">
+        <form action="" method="POST">
             <label for="name">Vaše meno</label>
             <input type="text" id="name" name="name" placeholder="Meno" autofocus>
             <input type="text" id="surname" name="surname" placeholder="Priezvisko">
