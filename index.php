552fd74f9765dd8b1e4d470be1feaf8fb61f09ab
miRx923 <m.kosaric@protonmail.com>
2022-11-12 10:42:42 +0100

    fgawgwaga

--- index.php
+++ index.php
@@ -1,97 +1,21 @@
 <?php
-    $name = $_POST['name'];
-    $surname = $_POST['surname'];
-    
-    if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
-        $nameError = "Tvoje krstné meno nie je zapísané správne";
-    } elseif (empty($_POST['name'])) {
-        $nameError = "Musíš zadať krstné meno";
-    }
-
-    if (!preg_match("/^[a-zA-Z-' ]*$/", $surname)) {
-        $surnameError = "Tvoje priezvisko nie je zapísané správne.";
-    } elseif (empty($_POST['surname'])) {
-        $surnameError = "Musíš zadať priezvisko";
-    }
-
-    $num = $_POST['num'];
-    $email = $_POST['email'];
-
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $emailError = "Email zapísaný v nesprávnom formáte.";
-    } elseif (empty($_POST['email'])) {
-        $emailError = "Musíš zadať email."
-    }
-
-    if (isset($_POST['isLiked'])) {
-        $isLiked = "Ano";
-    } elseif (isset($_POST['isNotLiked'])) {
-        $isLiked = "Nie";
-    } else {
-        $isLiked = "";
-    }
-
-    $favColor = $_POST['favColor'];
-    $opinion = $_POST['opinion'];
-
-    $text = "Meno: " . $name . " " . $surname . "\r\n";
-    $text .= "Telefonne cislo: " . $num . "\r\n";
-    $text .= "Email: " . $email . "\r\n";
-    $text .= "Paci sa mu/jej IPaIoT: " . $isLiked . "\r\n";
-    $text .= "Nazor na predmet: " . $opinion;
-
-    $data = fopen("data.txt", "w") or die("Unable to open file!");
-    fwrite($data, $text);
-
-    fclose($data);
-?>
-
-<html lang="sk">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-    <title>IPaIoT Web Form</title>
-</head>
-<body>
-    <div class="formDiv">
-        <h2 class="heading">IPAIOT WEB FORM</h2>
-        <form action="" method="POST">
-            <label for="name">Vaše meno</label>
-            <input type="text" id="name" name="name" placeholder="Meno" autofocus>
-            <input type="text" id="surname" name="surname" placeholder="Priezvisko">
-
-            <br><br>
-            <label for="num">Telefónne číslo</label>
-            <input type="tel" id="num" name="num" placeholder="0912345678">
-
-            <br><br>
-            <label for="email">Emailová adresa</label>
-            <input type="email" id="email" name="email" placeholder="meno@mail.com">
-
-            <br><br>
-            <label for="like">Páči sa ti IPaIoT?</label>
-            <input type="radio" id="like" name="isLiked" class="like">
-            <p class="ano">Áno</p>
-
-            <input type="radio" id="like" name="isNotLiked" class="disLike">
-            <p class="nie">Nie</p>
-
-            <br><br>
-            <label for="opinion">Názor na predmet</label>
-            <input type="text" id="opinion" name="opinion">
-
-            <br><br><br><br>
-            <hr>
-            <button class="submitButton" onclick="thanks()">ODOSLAŤ</button>
-        </form>
-    </div>
-</body>
-</html>
-
-<script type="text/javascript">
-    function thanks() {
-        alert("Ďakujem za vyplnenie dotazníka");
-    }
-</script>
+        
+        $sn1 = $_GET["a"];
+        $sn2 = $_GET["b"];
+        
+        $file1 = fopen("sensors.txt","w") or die("Unable to open file!");
+        $text1 = "a=" . $sn1 . " b=" . $sn2;
+        
+        fwrite($file1, $text1);
+        fclose($file1);
+        
+        $file2 = fopen("actuator.txt","w") or die("Unable to open file!");
+        $text2 = "Value from actuator. Save this value to actuator.txt";
+        fwrite($file2, $text2);
+        fclose($file2);
+        
+        $file3 = fopen("actuator.txt","r") or die ("Subor neexistuje");
+        $text3 = fread($file3,filesize("actuator.txt"));
+        echo $text3;
+        fclose($file3);
+    ?>
