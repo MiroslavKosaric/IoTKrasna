e2c45c1b973caed4bfceb7eac7300ca85ec0c4eb
miRx923 <m.kosaric@protonmail.com>
2022-11-12 10:55:19 +0100

    ghsghs

--- index.php
+++ index.php
@@ -48,3 +48,53 @@
 
     fclose($data);
 ?>
+
+<html lang="sk">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>IPaIoT Web Form</title>
+</head>
+<body>
+    <div class="formDiv">
+        <h2 class="heading">IPAIOT WEB FORM</h2>
+        <form action="" method="POST">
+            <label for="name">Vaše meno</label>
+            <input type="text" id="name" name="name" placeholder="Meno" autofocus>
+            <input type="text" id="surname" name="surname" placeholder="Priezvisko">
+
+            <br><br>
+            <label for="num">Telefónne číslo</label>
+            <input type="tel" id="num" name="num" placeholder="0912345678">
+
+            <br><br>
+            <label for="email">Emailová adresa</label>
+            <input type="email" id="email" name="email" placeholder="meno@mail.com">
+
+            <br><br>
+            <label for="like">Páči sa ti IPaIoT?</label>
+            <input type="radio" id="like" name="isLiked" class="like">
+            <p class="ano">Áno</p>
+
+            <input type="radio" id="like" name="isNotLiked" class="disLike">
+            <p class="nie">Nie</p>
+
+            <br><br>
+            <label for="opinion">Názor na predmet</label>
+            <input type="text" id="opinion" name="opinion">
+
+            <br><br><br><br>
+            <hr>
+            <button class="submitButton" onclick="thanks()">ODOSLAŤ</button>
+        </form>
+    </div>
+</body>
+</html>
+
+<script type="text/javascript">
+    function thanks() {
+        alert("Ďakujem za vyplnenie dotazníka");
+    }
+</script>
