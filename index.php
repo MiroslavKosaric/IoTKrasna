15935601d611743983f54bfe82992dd746e6828b
MiroslavKosaric <114644551+MiroslavKosaric@users.noreply.github.com>
2022-11-14 14:29:42 +0100

    Update index.php

--- index.php
+++ index.php
@@ -106,6 +106,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="icon.png" type="image/icon type">
     <link rel="stylesheet" type="text/css" href="style.css">
     <title>IPaIoT Web Form</title>
 </head>
